ad161b8c4579be835c8e3f74d8f8771a16315e68
Jiwon Choi <jwchoi@lablup.com>
2024-03-15 16:10:40 +0900

    Feat: 메인 페이지 필요 Widget 배치
    - 전반적인 UI 구조를 잡기 위하여 필요한 Antd Widget을 임시로 추가
    - 디자인 시스템에서 정의한 색상은 따로 `tailwimd.config.js` 파일에서 관리

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#4200ff",
+      },
+    },
   },
   plugins: [],
 };
