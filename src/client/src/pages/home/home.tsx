8c07e67adfa9d9c488f7bdcde9f47b0c759940ba
Jiwon Choi <jwchoi@lablup.com>
2024-04-03 01:11:07 +0900

    Feat: `HomePage` 좌우측 여백 영역 UI 구현 완료
    - 반응형 디자인을 고려하였을 때 스크린 영역을 fixed, absolute를 사용하여 위에 포개는 것보다 나누는 것이 좋다고 판단하였습니다.
    - 우측 여백이 좌측 여백의 `height` 값에 따라 유동적으로 변경되도록 구현하였습니다.

--- src/client/src/pages/home/home.tsx
+++ src/client/src/pages/home/home.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+import logoWhite from "../../assets/images/LogoWhite.svg";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const mainDivRef = useRef<HTMLDivElement>(null);
+  const [divHeight, setDivHeignt] = useState(0);
+
+  useEffect(() => {
+    setDivHeignt(mainDivRef.current?.clientHeight || 0);
+    console.log(divHeight);
+  }, []);
+
+  return (
+    <div className="flex">
+      {/* 좌측 영역 */}
+      <div className="flex flex-col h-screen">
+        <div ref={mainDivRef} className="bg-main pt-[55px] pb-[66px] px-[60px]">
+          <img src={logoWhite} />
+          <div className="flex flex-col w-[180px] mt-12 items-center">
+            <img className="w-20 h-20 rounded-full bg-white" src="" />
+            <p className="font-medium text-base text-white pt-5 pb-2">
+              (주)KM컴퍼니
+            </p>
+            <p className="text-white">kmofficial@gmail.com</p>
+          </div>
+        </div>
+        <div className="flex-grow bg-[#F0F2F5]"></div>
+      </div>
+      {/* 스크린 영역 */}
+      <div className="flex-grow"></div>
+      {/* 우측 여백 영역 */}
+      <div className="flex flex-col w-[60px]">
+        <div style={{ height: `${divHeight}px` }} className="bg-main" />
+        <div className="flex-grow bg-[#F0F2F5]" />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
