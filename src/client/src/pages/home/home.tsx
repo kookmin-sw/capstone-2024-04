a252eaefa162c3082c466e5b3c3396296f7adf24
Jiwon Choi <jwchoi@lablup.com>
2024-04-03 18:41:51 +0900

    Feat: `HomePage` 하단 메뉴 버튼 구현

--- src/client/src/pages/home/home.tsx
+++ src/client/src/pages/home/home.tsx
@@ -1,10 +1,71 @@
 import { useEffect, useRef, useState } from "react";
 import logoWhite from "../../assets/images/LogoWhite.svg";
 import infoCircle from "../../assets/icons/InfoCircle.svg";
+import clipboardTextClockBlacksub from "../../assets/icons/clipboard-text-clock-blacksub.svg";
+import clipboardTextClockWhitesub from "../../assets/icons/clipboard-text-clock-whitesub.svg";
+import chartTimelineBlacksub from "../../assets/icons/chart-timeline-blacksub.svg";
+import chartTimelineWhitesub from "../../assets/icons/chart-timeline-whitesub.svg";
+import cogBlacksub from "../../assets/icons/cog-blacksub.svg";
+import cogWhitesub from "../../assets/icons/cog-whitesub.svg";
+import fileCompareBlacksub from "../../assets/icons/file-compare-blacksub.svg";
+import fileCompareWhitesub from "../../assets/icons/file-compare-whitesub.svg";
+import listBoxBlacksub from "../../assets/icons/list-box-blacksub.svg";
+import listBoxWhitesub from "../../assets/icons/list-box-whitesub.svg";
+import uploadBlacksub from "../../assets/icons/upload-blacksub.svg";
+import uploadWhitesub from "../../assets/icons/upload-whitesub.svg";
+import accountBlacksub from "../../assets/icons/account-blacksub.svg";
+import accountWhitesub from "../../assets/icons/account-whitesub.svg";
+import viewDashboardBlacksub from "../../assets/icons/view-dashboard-blacksub.svg";
+import viewDashboardWhitesub from "../../assets/icons/view-dashboard-whitesub.svg";
+import MenuButton from "../../components/menu_button";
 
 const HomePage = () => {
   const mainDivRef = useRef<HTMLDivElement>(null);
   const [divHeight, setDivHeight] = useState(0);
+  const [currMenuIdx, setCurrMenuIdx] = useState(0);
+
+  const menuButtons = [
+    {
+      title: "대시보드",
+      iconWhiteSrc: viewDashboardWhitesub,
+      iconBlackSrc: viewDashboardBlacksub,
+    },
+    {
+      title: "요약",
+      iconWhiteSrc: chartTimelineWhitesub,
+      iconBlackSrc: chartTimelineBlacksub,
+    },
+    {
+      title: "비교",
+      iconWhiteSrc: fileCompareWhitesub,
+      iconBlackSrc: fileCompareBlacksub,
+    },
+    {
+      title: "히스토리",
+      iconWhiteSrc: clipboardTextClockWhitesub,
+      iconBlackSrc: clipboardTextClockBlacksub,
+    },
+    {
+      title: "광고등록",
+      iconWhiteSrc: uploadWhitesub,
+      iconBlackSrc: uploadBlacksub,
+    },
+    {
+      title: "광고목록",
+      iconWhiteSrc: listBoxWhitesub,
+      iconBlackSrc: listBoxBlacksub,
+    },
+    {
+      title: "내 계정",
+      iconWhiteSrc: accountWhitesub,
+      iconBlackSrc: accountBlacksub,
+    },
+    {
+      title: "설정",
+      iconWhiteSrc: cogWhitesub,
+      iconBlackSrc: cogBlacksub,
+    },
+  ];
 
   useEffect(() => {
     const height = mainDivRef.current?.clientHeight || 0;
@@ -25,7 +86,17 @@ const HomePage = () => {
             <p className="text-white">kmofficial@gmail.com</p>
           </div>
         </div>
-        <div className="flex-grow bg-[#F0F2F5]"></div>
+        <div className="grid grid-cols-2 gap-y-3 gap-x-4 flex-grow bg-[#F0F2F5] p-[60px]">
+          {menuButtons.map((button, index) => (
+            <MenuButton
+              title={button.title}
+              iconBlackSrc={button.iconBlackSrc}
+              iconWhiteSrc={button.iconWhiteSrc}
+              isActive={index === currMenuIdx}
+              onClick={() => setCurrMenuIdx(index)}
+            />
+          ))}
+        </div>
       </div>
       {/* 스크린 영역 */}
       <div
