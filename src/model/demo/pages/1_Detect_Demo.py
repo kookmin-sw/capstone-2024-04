600dd9f29b7f6b78d3af2eebe99fc6cc99528fe9
KH <ganghe74@gmail.com>
2024-04-04 16:47:18 +0900

    Feat: 모델 데모 추가

---
+++ src/model/demo/pages/1_Detect_Demo.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
+
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+from pipeline import Pipeline
+
+st.write('# 객체 검출 데모 페이지')
+uploaded_file = st.file_uploader("이미지 파일 업로드")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    image = np.array(image)
+
+    pipeline = Pipeline()
+    results = pipeline.detect(image)
+    assert len(results) == 1
+
+    for result in results:
+        plotted_iamge = result.plot()
+
+    st.image(plotted_iamge)
