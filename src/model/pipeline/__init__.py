d18723a43ebd309dc40d9d5c05d446d352c5efdf
KH <ganghe74@gmail.com>
2024-04-04 13:54:55 +0900

    Feat: 파이프라인에 PAR 클래스 추가
    아직 개별 클래스별로만 동작 확인하고, 조립은 안 되어있는 상태.
    Pipeline 클래스 구현을 해야함
    
    ref: https://github.com/kookmin-sw/capstone-2024-04/issues/23, https://github.com/kookmin-sw/capstone-2024-04/issues/27

--- src/model/pipeline/__init__.py
+++ src/model/pipeline/__init__.py
@@ -1,3 +1,4 @@
 from .pipeline import Pipeline
 from .tracker import Tracker
 from .gaze import Gaze
+from .par import PAR
---
+++ src/model/pipeline/par.py
@@ -0,0 +1,139 @@
+import os
+
+import cv2
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
+from torchvision import models
+from torchvision import transforms as T
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    # print(classname)
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+        init.constant_(m.bias.data, 0.0)
+    elif classname.find('BatchNorm1d') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        init.normal_(m.weight.data, std=0.001)
+        init.constant_(m.bias.data, 0.0)
+
+
+# Defines the new fc layer and classification layer
+# |--Linear--|--bn--|--relu--|--Linear--|
+class ClassBlock(nn.Module):
+    def __init__(self, input_dim, class_num=1, activ='sigmoid', num_bottleneck=512):
+        super(ClassBlock, self).__init__()
+
+        add_block = []
+        add_block += [nn.Linear(input_dim, num_bottleneck)]
+        add_block += [nn.BatchNorm1d(num_bottleneck)]
+        add_block += [nn.LeakyReLU(0.1)]
+        add_block += [nn.Dropout(p=0.5)]
+
+        add_block = nn.Sequential(*add_block)
+        add_block.apply(weights_init_kaiming)
+
+        classifier = []
+        classifier += [nn.Linear(num_bottleneck, class_num)]
+        if activ == 'sigmoid':
+            classifier += [nn.Sigmoid()]
+        elif activ == 'softmax':
+            classifier += [nn.Softmax()]
+        elif activ == 'none':
+            classifier += []
+        else:
+            raise AssertionError("Unsupported activation: {}".format(activ))
+        classifier = nn.Sequential(*classifier)
+        classifier.apply(weights_init_classifier)
+
+        self.add_block = add_block
+        self.classifier = classifier
+
+    def forward(self, x):
+        x = self.add_block(x)
+        x = self.classifier(x)
+        return x
+
+
+class Backbone_nFC(nn.Module):
+    def __init__(self, class_num, model_name='resnet50_nfc'):
+        super(Backbone_nFC, self).__init__()
+        self.model_name = model_name
+        self.backbone_name = model_name.split('_')[0]
+        self.class_num = class_num
+
+        model_ft = getattr(models, self.backbone_name)(pretrained=True)
+        if 'resnet' in self.backbone_name:
+            model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            model_ft.fc = nn.Sequential()
+            self.features = model_ft
+            self.num_ftrs = 2048
+        elif 'densenet' in self.backbone_name:
+            model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            model_ft.fc = nn.Sequential()
+            self.features = model_ft.features
+            self.num_ftrs = 1024
+        else:
+            raise NotImplementedError
+        
+        for c in range(self.class_num):
+            self.__setattr__('class_%d' % c, ClassBlock(input_dim=self.num_ftrs, class_num=1, activ='sigmoid') )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        pred_label = [self.__getattr__('class_%d' % c)(x) for c in range(self.class_num)]
+        pred_label = torch.cat(pred_label, dim=1)
+        return pred_label
+    
+transforms = T.Compose([
+    T.ToTensor(),
+    T.Resize(size=(288, 144)),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+
+class PAR:
+    PAR_WEIGHT_PATH = os.getenv('PAR_WEIGHT_PATH')
+    
+    def __init__(self):
+        self.model = Backbone_nFC(30)
+        if PAR.PAR_WEIGHT_PATH is not None:
+            self.model.load_state_dict(torch.load(PAR.PAR_WEIGHT_PATH))
+        else:
+            print('PAR model weight is not given.')
+        self.model.eval()
+    
+    @torch.no_grad()
+    def predict(self, image_path):
+        im = cv2.imread(image_path)
+        im = transforms(im)
+        im = im.unsqueeze(0)
+        out = self.model.forward(im)
+        age_idx = out[0,:4].argmax()
+        age = ['young', 'teenager', 'adult', 'old'][age_idx]
+        gender = 'male' if out[0, 12] < 0.5 else 'female'
+        return {
+            'age': age,
+            'gender': gender
+        }
+        
+if __name__ == '__main__':
+    import os
+    assert os.path.exists('lena.jpg')
+    
+    par = PAR()
+    result = par.predict('lena.jpg')
+    print(result)
