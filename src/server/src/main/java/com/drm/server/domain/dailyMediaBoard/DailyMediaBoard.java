ada12684c3ccf9ce46a9303a66eda517bedefd99
Juliandocode <53322908+donggook-me@users.noreply.github.com>
2024-04-07 19:08:13 +0900

    change entity column #66

--- src/server/src/main/java/com/drm/server/domain/dailyMediaBoard/DailyMediaBoard.java
+++ src/server/src/main/java/com/drm/server/domain/dailyMediaBoard/DailyMediaBoard.java
@@ -19,14 +19,30 @@ public class DailyMediaBoard {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mediaDataId;
 
+    // 전체 포착된 사람 수
     @Column
     private Long totalPeopleCount;
 
+    // 시간당 관심을 표현한 사람 수 (0시 - 12시 까지)
     @Convert(converter = DataConverter.class)
     private List<Long> hourlyPassedCount;
+
+    // 시간당 포착된 사람 수 (0시 - 12시 까지)
     @Convert(converter = DataConverter.class)
     private List<Long> hourlyInterestedCount;
 
+
+    // 관심을 표현한 남자의 인원 수
+    private Long maleInterestCnt;
+    // 관심을 표현한 여자의 인원 수
+    private Long femaleInterestCnt;
+    // 집계된 남자의 인원 수
+    private Long maleCnt;
+
+    // 해당 광고 평균 시청 시간
+    private float avgStaringTime;
+    // 해당 광고 평균 시청 남자의 비율 (0.0 ~ 100.0)
     private float avgMale;
+    // 해당 광고 평균 시청 나이 (0.0 ~ F )
     private float avgAge;
 }
