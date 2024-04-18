77b28fc8f0988e7325a632c588b9caf48be7b314
Jiwon Choi <jwchoi@lablup.com>
2024-04-18 15:54:51 +0900

    Chore: `ErrorResponse` interface 수정

--- src/client/src/interfaces/interface.ts
+++ src/client/src/interfaces/interface.ts
@@ -14,7 +14,7 @@ export interface ErrorResponse {
     status: number,
     divisionCode: string,
     resultMsg: string,
-    errors: Record<string, unknown>,
+    errors: {},
     reason: string,
 }
 
