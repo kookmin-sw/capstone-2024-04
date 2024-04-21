31502d146b2fd7000d99e40fb9b70fa4f4f0a218
Jiwon Choi <jwchoi@lablup.com>
2024-04-21 17:25:29 +0900

    Refactor: 필수 매개변수 color에 대한 처리 수정

--- src/client/src/components/text.tsx
+++ src/client/src/components/text.tsx
@@ -5,132 +5,52 @@ interface TextProps {
 
 /** 18px, Regular */
 const Headline1 = ({ text, color }: TextProps) => {
-  return (
-    <h2
-      className={`text-lg font-normal ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h2>
-  );
+  return <h2 className={`text-lg font-normal text-${color}`}>{text}</h2>;
 };
 
 /** 18px, Medium */
 const Headline2 = ({ text, color }: TextProps) => {
-  return (
-    <h2
-      className={`text-lg font-medium ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h2>
-  );
+  return <h2 className={`text-lg font-medium text-${color}`}>{text}</h2>;
 };
 
 /** 18px, SemiBold */
 const Headline3 = ({ text, color }: TextProps) => {
-  return (
-    <h2
-      className={`text-lg font-semibold ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h2>
-  );
+  return <h2 className={`text-lg font-semibold text-${color}`}>{text}</h2>;
 };
 
 /** 16px, Regular */
 const Subtitle1 = ({ text, color }: TextProps) => {
-  return (
-    <h3
-      className={`text-base font-normal ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h3>
-  );
+  return <h3 className={`text-base font-normal text-${color}`}>{text}</h3>;
 };
 
 /** 16px, Medium */
 const Subtitle2 = ({ text, color }: TextProps) => {
-  return (
-    <h3
-      className={`text-base font-medium ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h3>
-  );
+  return <h3 className={`text-base font-medium text-${color}`}>{text}</h3>;
 };
 
 /** 16px, SemiBold */
 const Subtitle3 = ({ text, color }: TextProps) => {
-  return (
-    <h3
-      className={`text-base font-semibold ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </h3>
-  );
+  return <h3 className={`text-base font-semibold text-${color}`}>{text}</h3>;
 };
 
 /** 14px, Regular */
 const Body1 = ({ text, color }: TextProps) => {
-  return (
-    <p
-      className={`text-sm font-normal ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </p>
-  );
+  return <p className={`text-sm font-normal text-${color}`}>{text}</p>;
 };
 
 /** 14px, Medium */
 const Body2 = ({ text, color }: TextProps) => {
-  return (
-    <p
-      className={`text-sm font-medium ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </p>
-  );
+  return <p className={`text-sm font-medium text-${color}`}>{text}</p>;
 };
 
 /** 12px, Regular */
 const Caption1 = ({ text, color }: TextProps) => {
-  return (
-    <p
-      className={`text-xs font-normal ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </p>
-  );
+  return <p className={`text-xs font-normal text-${color}`}>{text}</p>;
 };
 
 /** 12px, Medium */
 const Caption2 = ({ text, color }: TextProps) => {
-  return (
-    <p
-      className={`text-xs font-medium ${
-        color ? `text-${color}` : "text-black"
-      }`}
-    >
-      {text}
-    </p>
-  );
+  return <p className={`text-xs font-medium text-${color}`}>{text}</p>;
 };
 
 export {
