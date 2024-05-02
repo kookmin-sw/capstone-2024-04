57e5f7453bc0cf18ebe9b6c4738dd8a1ee627f79
Jiwon Choi <jwchoi@lablup.com>
2024-05-02 15:44:05 +0900

    Feat: `patchPassword`, `verifyPassword` 추가

--- src/client/src/api/user.ts
+++ src/client/src/api/user.ts
@@ -1,8 +1,27 @@
 import privateApi from "./private_api"
 
+interface patchPasswordProps {
+    password: string,
+    updatePassword: string,
+}
+
+interface verifyPasswordProps {
+    password: string,
+}
+
 export const patchProfile = async (formData: FormData) => {
     const response = await privateApi.patch('/api/v1/user/profile', formData, {headers: {
         "Content-Type": "multipart/form-data",
     }}).catch((err) => {return err.response});
+    return response;
+}
+
+export const patchPassword = async ({password, updatePassword}: patchPasswordProps) => {
+    const response = await privateApi.patch('/api/v1/user/password', {password, updatePassword}).catch((err) => {return err.response})
+    return response;
+}
+
+export const verifyPassword = async ({password}: verifyPasswordProps) => {
+    const response = await privateApi.patch('/api/v1/user/verify-password', {password}).catch((err) => {return err.response})
     return response;
 }
