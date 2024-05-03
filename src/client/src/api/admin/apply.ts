d00ec1da1e937f8788797b609d9f6410227011c8
Jiwon Choi <jwchoi@lablup.com>
2024-05-03 17:39:22 +0900

    Feat: 어드민 신청 관리 API 추가

---
+++ src/client/src/api/admin/apply.ts
@@ -0,0 +1,24 @@
+import privateApi from "../private_api"
+
+export interface patchApplyProps {
+    applyId: Array<number>,
+    status: ApplyStatus, 
+}
+
+export enum ApplyStatus {
+    WAITING,
+    REJECT,
+    ACCEPT,
+}
+
+export const getApply = async () => {
+    const response = await privateApi.get('/api/v2/apply').catch((err) => {return err.response});
+    return response;
+}
+
+export const patchApply = async ({applyId, status}: patchApplyProps) => {
+    const response = await privateApi.patch('/api/v2/apply', {applyId, status}).catch((err) => {
+        return err.response;
+    })
+    return response;
+}
