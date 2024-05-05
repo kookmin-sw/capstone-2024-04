6cf9b44cd48521cd28ba52fcf333b9364d5b2749
Juliandocode <53322908+donggook-me@users.noreply.github.com>
2024-05-05 22:42:53 +0900

    #129 fps 반영한 평균 시간 계산 코드 수정

--- src/server/src/main/java/com/drm/server/service/DailyMediaBoardService.java
+++ src/server/src/main/java/com/drm/server/service/DailyMediaBoardService.java
@@ -38,9 +38,9 @@ public class DailyMediaBoardService {
         dailyMediaBoard.updateAvgAge(detectedFace.getAge());
 //        관심이 있다
         if(detectedFace.getFaceCaptureCnt() > 0){
-            dailyMediaBoard.updateAvgStaringTime(detectedFace.getFaceCaptureCnt()); //평균시선 시간
+            dailyMediaBoard.updateAvgStaringTime(detectedFace.getFaceCaptureCnt(), detectedFace.getFps()); //평균시선 시간
             dailyMediaBoard.updateInterestedAgeRangeCount(ageRange); //연령대별 관심인구수
-            dailyMediaBoard.updateHourlyAvgStaringTime(dataHour, detectedFace.getFaceCaptureCnt()); //시간별 평균시선
+            dailyMediaBoard.updateHourlyAvgStaringTime(dataHour, detectedFace.getFaceCaptureCnt(), detectedFace.getFps()); //시간별 평균시선
             dailyMediaBoard.addHourlyInterestedCount(dataHour); // 시간별 관심인구
 
             if(detectedFace.isMale()){
