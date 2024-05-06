43b0e2141a0bf3333adf970478a80c1bcf9d0a07
Jiwon Choi <jwchoi@lablup.com>
2024-05-06 14:05:28 +0900

    Feat: `StatusBadge` 집행 예정, 집행중, 집행 종료 구분 추가

--- src/client/src/components/status_badge.tsx
+++ src/client/src/components/status_badge.tsx
@@ -8,15 +8,18 @@ export interface StatusBadgeProps {
 const StatusBadge = ({ status, date }: StatusBadgeProps) => {
   const [color, setColor] = useState<string>("");
   const [text, setText] = useState<string>("");
+  const startDate = new Date(date[0]);
+  const endDate = new Date(date[1]);
+  const currentDate = new Date();
 
   useEffect(() => {
     if (status === "REJECT") {
       setColor("bg-[#808080]");
       setText("등록거절");
-    } else if (status === "ACCEPT") {
+    } else if (status === "ACCEPT" && currentDate < startDate) {
       setColor("bg-[#30B6A7]");
       setText("집행예정");
-    } else if (status === "ACCEPT") {
+    } else if (status === "ACCEPT" && currentDate > endDate) {
       setColor("bg-[#EF816A]");
       setText("집행종료");
     } else if (status === "ACCEPT") {
