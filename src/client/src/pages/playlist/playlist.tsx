a79af3cfc05d4efe2f80cca9b12f84f1403df09c
Jiwon Choi <jwchoi@lablup.com>
2024-05-15 00:13:24 +0900

    Feat: `BroadCasting` API 모듈화

--- src/client/src/pages/playlist/playlist.tsx
+++ src/client/src/pages/playlist/playlist.tsx
@@ -1,5 +1,35 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  GetDailyPlayListProps,
+  getDailyPlayList,
+} from "../../api/none/broadcasting";
+import { LocationInfo, MediaInfo } from "../../interfaces/interface";
+
+interface TodayList {
+  playListId: number;
+  broadCasting: boolean;
+  location: LocationInfo;
+  media: MediaInfo;
+}
+
 const PlayListScreen = () => {
-  return <></>;
+  const { locationId } = useParams();
+
+  const loadDailyPlayList = async ({ locationId }: GetDailyPlayListProps) => {
+    const result = await getDailyPlayList({ locationId });
+    if (result.status === 200) {
+      console.log(result.data.data);
+    }
+  };
+
+  useEffect(() => {
+    if (locationId) {
+      loadDailyPlayList({ locationId: parseInt(locationId) });
+    }
+  }, [locationId]);
+
+  return <>{locationId}</>;
 };
 
 export default PlayListScreen;
