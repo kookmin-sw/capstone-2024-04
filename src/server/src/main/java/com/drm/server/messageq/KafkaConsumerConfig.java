17742aa356fa27e9b33553ba3084ec49f11047e0
Juliandocode <53322908+donggook-me@users.noreply.github.com>
2024-05-24 17:24:56 +0900

    #170 improve Kafka Listener
    1) Multi threading 1 -> 5
    2) Batch Listener

--- src/server/src/main/java/com/drm/server/messageq/KafkaConsumerConfig.java
+++ src/server/src/main/java/com/drm/server/messageq/KafkaConsumerConfig.java
@@ -16,16 +16,18 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
-//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://43.203.218.109:29092"); // 배포 환경 설정
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:29092");
+//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://43.203.218.109:29092"); // 배포 환경 설정
 //        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "175.45.203.201:9092"); // 배포 환경 설정
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-
+        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
         return new DefaultKafkaConsumerFactory<>(properties);
     }
     @Bean
@@ -33,7 +35,8 @@ public class KafkaConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory
                 = new ConcurrentKafkaListenerContainerFactory<>();
         kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
-
+        kafkaListenerContainerFactory.setConcurrency(5);
+        kafkaListenerContainerFactory.setBatchListener(true);
         return kafkaListenerContainerFactory;
     }
 }
