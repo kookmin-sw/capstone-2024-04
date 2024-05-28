bf7c904a2b288f469714a6f550b49c6c03295aca
Jiwon Choi <jwchoi@lablup.com>
2024-05-28 21:59:49 +0900

    Fix: `ReactPlayer` 다음 영상 자동 미재생 현상 수정

--- src/client/src/pages/playlist/playlist.tsx
+++ src/client/src/pages/playlist/playlist.tsx
@@ -13,26 +13,13 @@ interface TodayList {
   media: MediaInfo;
 }
 
-// const getCurrentTimeList = () => {
-//   const now = new Date();
-//   return [
-//     now.getFullYear(),
-//     now.getMonth() + 1,
-//     now.getDate(),
-//     now.getHours(),
-//     now.getMinutes(),
-//     now.getSeconds(),
-//     now.getMilliseconds(),
-//     0,
-//     0,
-//   ];
-// };
-
 const PlayListScreen = () => {
   const { locationId } = useParams();
   const [isLoad, setIsLoad] = useState<boolean>(false);
   const [videoList, setVideoList] = useState<TodayList[]>([]);
   const [currentVideoIndex, setCurrentVideoIndex] = useState<number>(0);
+  const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
+  const [refreshKey, setRefreshKey] = useState<number>(0);
 
   const loadDailyPlayList = async (locationId: number) => {
     try {
@@ -53,27 +40,30 @@ const PlayListScreen = () => {
     }
   }, [locationId]);
 
+  useEffect(() => {
+    if (videoList.length > 0) {
+      setVideoUrl(videoList[currentVideoIndex].media.mediaLink);
+    }
+  }, [currentVideoIndex, videoList]);
+
   const handleVideoEnded = () => {
     setCurrentVideoIndex(
       currentVideoIndex + 1 === videoList.length ? 0 : currentVideoIndex + 1
     );
+    setRefreshKey((prev) => prev + 1);
   };
-
-  useEffect(() => {
-    if (videoList.length > 0) {
-      // 서버 측에 다음 동영상으로 넘어갔음을 알림
-    }
-  }, [currentVideoIndex, videoList]);
 
   return (
     <div className="w-screen h-screen">
       {isLoad ? (
         <ReactPlayer
+          key={refreshKey}
           width={"100%"}
           height={"100%"}
-          url={videoList[currentVideoIndex].media.mediaLink}
+          url={videoUrl}
           controls
           playing
+          muted
           onEnded={handleVideoEnded}
         />
       ) : (
